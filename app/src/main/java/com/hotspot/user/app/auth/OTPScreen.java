13ec1c23586d76118bcc1b691888aeb6a002955f
RohitSingh <fgroup.rohit@gmail.com>
2020-05-20 03:39:43 +0530

    Hotspots

--- app/src/main/java/com/hotspot/user/app/auth/OTPScreen.java
+++ app/src/main/java/com/hotspot/user/app/auth/OTPScreen.java
@@ -2,11 +2,14 @@ package com.hotspot.user.app.auth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.SpannableString;
 import android.text.style.UnderlineSpan;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.hotspot.user.app.DashboardActivity;
 import com.hotspot.user.app.R;
 
 public class OTPScreen extends AppCompatActivity {
@@ -18,9 +21,11 @@ public class OTPScreen extends AppCompatActivity {
         setContentView(
                 R.layout.activity_o_t_p_screen);
 
-        TextView textView = (TextView) findViewById(R.id.txtResendOtp);
+        TextView textView = findViewById(R.id.txtResendOtp);
         SpannableString content = new SpannableString("Content");
         content.setSpan(new UnderlineSpan(), 0, content.length(), 0);
         textView.setText(content);
+
+        findViewById(R.id.txtConfirmOTP).setOnClickListener(v -> Toast.makeText(getApplicationContext(),"Page is under construction",Toast.LENGTH_LONG).show());
     }
 }
