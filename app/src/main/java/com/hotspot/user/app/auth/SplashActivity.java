1fce6073e778db00aa4a42d18d6ebad26a17e5d4
Rohit Singh <59027452+Rohit130508@users.noreply.github.com>
2020-05-28 16:00:34 +0530

    UNIGlobeNidhi

--- app/src/main/java/com/hotspot/user/app/auth/SplashActivity.java
+++ app/src/main/java/com/hotspot/user/app/auth/SplashActivity.java
@@ -5,30 +5,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import com.hotspot.user.app.MainActivity;
 import com.hotspot.user.app.R;
 
 public class SplashActivity extends AppCompatActivity {
 
+    Animation downtoup;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setTheme(R.style.AppTheme);
         setContentView(R.layout.activity_splash);
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
+        downtoup = AnimationUtils.loadAnimation(this,R.anim.downtoup);
+        ImageView imgLogo = findViewById(R.id.imgLogo);
+        imgLogo.setAnimation(downtoup);
 
-
-                // This method will be executed once the timer is over
-
-
-                Intent i = new Intent(SplashActivity.this, LoginActivity.class);
-                SplashActivity.this.startActivity(i);
-                SplashActivity.this.finish();
-            }
+        new Handler().postDelayed(() -> {
+            // This method will be executed once the timer is over
+            Intent i = new Intent(SplashActivity.this, LoginActivity.class);
+            startActivity(i);
+            finish();
         }, 5000);
     }
 }
--- app/src/main/java/com/hotspot/user/app/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/hotspot/user/app/ui/dashboard/DashboardFragment.java
@@ -5,10 +5,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.hotspot.user.app.DashboardActivity;
 import com.hotspot.user.app.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -20,6 +25,7 @@ import androidx.recyclerview.widget.RecyclerView;
 public class DashboardFragment extends Fragment {
 
     private DashboardViewModel dashboardViewModel;
+    ArrayList<ModalMainCat>  arrayList;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -32,8 +38,7 @@ public class DashboardFragment extends Fragment {
         final RecyclerView rvMainCat = root.findViewById(R.id.rvMainCat);
         rvMainCat.setLayoutManager(new GridLayoutManager(getActivity(),3));
 
-        MainCatAdapter mainCatAdapter = new MainCatAdapter();
-        rvMainCat.setAdapter(mainCatAdapter);
+
 
 
         final RecyclerView rvSlider = root.findViewById(R.id.rvSlider);
@@ -42,6 +47,40 @@ public class DashboardFragment extends Fragment {
         SliderAdapter sliderAdapter = new SliderAdapter();
         rvSlider.setAdapter(sliderAdapter);
 
+        arrayList = new ArrayList<>();
+
+        ModalMainCat cat = new ModalMainCat();
+        cat.setName("RD");
+        cat.setIcon(R.drawable.ic_ma);
+        arrayList.add(cat);
+
+        ModalMainCat cat2 = new ModalMainCat();
+        cat.setName("DD");
+        cat.setIcon(R.drawable.ic_ma);
+        arrayList.add(cat2);
+
+        ModalMainCat cat3 = new ModalMainCat();
+        cat.setName("Saving");
+        cat.setIcon(R.drawable.ic_ma);
+        arrayList.add(cat3);
+
+        ModalMainCat cat4 = new ModalMainCat();
+        cat.setName("RD");
+        cat.setIcon(R.drawable.ic_ma);
+        arrayList.add(cat4);
+
+        ModalMainCat cat5 = new ModalMainCat();
+        cat.setName("DD");
+        cat.setIcon(R.drawable.ic_ma);
+        arrayList.add(cat5);
+
+        ModalMainCat cat6 = new ModalMainCat();
+        cat.setName("Saving");
+        cat.setIcon(R.drawable.ic_ma);
+        arrayList.add(cat6);
+
+        MainCatAdapter mainCatAdapter = new MainCatAdapter(arrayList);
+        rvMainCat.setAdapter(mainCatAdapter);
 
         return root;
     }
@@ -79,6 +118,12 @@ public class DashboardFragment extends Fragment {
     class MainCatAdapter extends RecyclerView.Adapter<MainCatAdapter.ViewHolder>
     {
 
+        ArrayList<ModalMainCat> arrayList;
+
+        public MainCatAdapter(ArrayList<ModalMainCat> arrayList) {
+            this.arrayList = arrayList;
+        }
+
         @NonNull
         @Override
         public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -89,20 +134,50 @@ public class DashboardFragment extends Fragment {
         @Override
         public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
 
-            holder.linClick.setOnClickListener(v -> startActivity(new Intent(getActivity(),TaxiBooking.class)));
+            ModalMainCat cat = arrayList.get(position);
+//            holder.txtTitle.setText(cat.getName());
+//            Picasso.with(getContext()).load(cat.getIcon()).into(holder.imgIcon);
+//            holder.linClick.setOnClickListener(v -> startActivity(new Intent(getActivity(),TaxiBooking.class)));
         }
 
         @Override
         public int getItemCount() {
-            return 9;
+            return arrayList.size();
         }
 
         class ViewHolder extends RecyclerView.ViewHolder {
             LinearLayout linClick ;
+            ImageView imgIcon;
+            TextView txtTitle;
+
             ViewHolder(@NonNull View itemView) {
                 super(itemView);
                 linClick = itemView.findViewById(R.id.linClick);
+                imgIcon = itemView.findViewById(R.id.imgIcon);
+                txtTitle = itemView.findViewById(R.id.txtTitle);
             }
         }
+    }
+
+    class ModalMainCat
+    {
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getIcon() {
+            return icon;
+        }
+
+        public void setIcon(int icon) {
+            this.icon = icon;
+        }
+
+        public String name;
+        public int icon;
     }
 }
