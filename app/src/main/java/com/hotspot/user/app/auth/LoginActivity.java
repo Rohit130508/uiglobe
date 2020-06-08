f43fc87f56d4a4c1b7c95ae6fbab5f7ca6bd510b
Rohit Singh <59027452+Rohit130508@users.noreply.github.com>
2020-06-08 13:27:33 +0530

    UNIGlobeNidhi

--- app/src/main/java/com/hotspot/user/app/auth/LoginActivity.java
+++ app/src/main/java/com/hotspot/user/app/auth/LoginActivity.java
@@ -2,12 +2,24 @@ package com.hotspot.user.app.auth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.hotspot.user.app.R;
+import com.hotspot.user.app.utils.AppUrls;
+import com.hotspot.user.app.utils.Utils;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText edtMobileNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +27,29 @@ public class LoginActivity extends AppCompatActivity {
         setTheme(R.style.AppTheme);
         setContentView(R.layout.activity_login);
 
+        initView();
+
         findViewById(R.id.cardNavigate).setOnClickListener(v-> startActivity(new Intent(this,OTPScreen.class)));
+    }
+
+    void initView()
+    {
+        edtMobileNumber = findViewById(R.id.edtMobileNumber);
+
+        if(Utils.isNetworkAvailable(this))
+        executeMethods();
+    }
+
+    void executeMethods()
+    {
+
+        StringRequest request = new StringRequest(Request.Method.GET, AppUrls.checkPhone, response -> {
+
+        }, error -> {
+
+        });
+
+        RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+        queue.add(request);
     }
 }
---
+++ app/src/main/java/com/hotspot/user/app/utils/AppUrls.java
@@ -0,0 +1,9 @@
+package com.hotspot.user.app.utils;
+
+public class AppUrls {
+
+    public static String BaseUrl = "http://uniglobe.flyaway.co.in/api/";
+
+
+    public static String checkPhone = BaseUrl+"UserId/Validation";
+}
